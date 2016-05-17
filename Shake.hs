c75e08c7efe378b2acb7085a26bc6f8550619db8
Brent Yorgey <byorgey@gmail.com>
2016-05-17 10:58:15 -0500

    fix Shake.hs

--- Shake.hs
+++ Shake.hs
@@ -8,7 +8,7 @@ pdflatex = "pdflatex"
 main :: IO ()
 main = shake shakeOptions $ do
 
-    want ["GCBP.pdf"]
+    want ["GCBP-paper.pdf"]
 
     "*.tex" %> \output -> do
         let input = replaceExtension output "lhs"
