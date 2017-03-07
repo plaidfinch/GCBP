fbbccd8d4ab66d2d22403d576bf52562c9776372
Brent Yorgey <byorgey@gmail.com>
2017-03-07 14:09:20 -0600

    testing

--- Test.hs
+++ Test.hs
@@ -2,4 +2,8 @@ import           Bijections
 import           Diagrams.Backend.Rasterific.CmdLine
 import           Diagrams.Prelude
 
-main = defaultMain $ (drawBComplex bc01')
+main = defaultMain $ drawBComplex (bij01'c)
+  where
+    cm = orbitsToColorMap [green, red, yellow, pink, orange] (bijToRel bij0)
+    bij0c = colorBij cm bij0
+    bij01'c = bc01 +- (reversing bij0c +++ empty) -.. (a0 +++ a1)
