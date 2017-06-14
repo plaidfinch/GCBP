08dfa2cb909023ca56f940677a1e155372f68964
Brent Yorgey <byorgey@gmail.com>
2017-06-13 21:18:31 -0500

    add QuickCheck to sandbox

--- sandbox.sh
+++ sandbox.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cabal sandbox init
-cabal install shake -fpgf -frasterific diagrams diagrams-builder random-shuffle
+cabal install shake -fpgf -frasterific diagrams diagrams-builder random-shuffle QuickCheck
